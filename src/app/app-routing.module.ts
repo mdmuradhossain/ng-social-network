621fe38b50a1980fa717bf0f5cb0eab4787d8d1e
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-12-17 20:37:30 +0600

    redirectTo route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { CreateAccountComponent } from './pages/create-account/create-account.co
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full',
+  // },
   {
     path: 'login',
     component: LoginComponent,
